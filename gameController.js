994681365b5f7a44d65c9abfed0a3de7ea41f029
Jana Borchardt <Jana.Borchardt@uni-duesseldorf.de>
2016-11-03 14:53:27 +0100

    javascript basic structure and create js import

---
+++ gameController.js
@@ -0,0 +1,29 @@
+
+window.alert("Welcome to PrisonSnake! Start game");
+
+stage = new createjs.Stage("myCanvas");
+
+//NameSpacing ParaS6 (= Package, to avoid name conflicts)
+var ParaS6 = {
+        PlayingField : function () {
+
+        }
+}
+
+var ParaS6 = {
+    PlayingFieldView : function () {
+
+    }
+}
+
+var ParaS6 = {
+    PrisonSnake : function () {
+
+    }
+}
+
+var ParaS6 = {
+    PrisonSnakeView : function () {
+
+    }
+}
