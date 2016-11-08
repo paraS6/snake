32f4f9a79069ddf532e337ba7d07b0ab67cbd7ea
Jana Borchardt <Jana.Borchardt@uni-duesseldorf.de>
2016-11-08 14:50:42 +0100

    grid aufgebaut als 2D Array und erste drawFunktion

--- js/gameController.js
+++ js/gameController.js
@@ -1,17 +1,30 @@
 
 var stage = new createjs.Stage("myCanvas");
-const WIDTH = stage.canvas.width - 300; //Breite vom Spielfeld
-const HEIGHT = stage.canvas.height - 100;   // Hoehe vom Spielfeld
+
+var SNAKE_HEAD = 2;
+const WIDTH = stage.width - 300; //Breite vom Spielfeld
+const HEIGHT = stage.height - 100;   // Hoehe vom Spielfeld
 const CELL = 20;    //Zellengroesse
-var grid = new Array((WIDTH/CELL)*(HEIGHT/CELL));
-const EMPTY = 0;  //leeres Feld
-const SNAKE_MEMBER = 1;    //Felder von Schlange besetzt
+var EMPTY = 0;  //leeres Feld
+    //Schlangenkopf
+const SNAKE_BODY = 1;
+
 var playingfieldImg = new createjs.Bitmap("img/dummyGround.jpg");
 
 function start() {
-    var field = new Snake.Views.PlayingFieldView();
-    field.drawPlayingField();
+    //kreiere 2D Array
+    var grid = new Array(35);
+    for(var i = 0; i < 35; i++)
+        grid[i] = new Array(20);
 
+    var field = new Snake.Views.PlayingFieldView();
+    var prisonSnake = new Snake.Models.PrisonSnake();
+    var prisonSnakeView = new Snake.Views.PrisonSnakeView();
+    //Startsetting
+    prisonSnake.startCoords(grid);
+    field.drawPlayingField();
+    prisonSnakeView.drawSnake(grid);
+    stage.update();
 }
 
 //NameSpacing ParaS6 (= Package, to avoid name conflicts)
@@ -36,7 +49,15 @@ var Snake = {
             this.setLevelSpeed = function (newLevelSpeed) {return _levelSpeed = newLevelSpeed; };
         },
         PrisonSnake: function () {
-            this.randomStart = function () {
+            this.startCoords = function (grid) {
+                for(var i = 0; i< grid.length; i++){
+                    for(var j = 0; j< grid.length; j++){
+                        grid[i][j]= EMPTY;
+                    }
+                }
+                //Set Head
+                window.alert(grid.length);
+                grid[5][5]= SNAKE_HEAD;
             }
         }
     },
@@ -46,14 +67,24 @@ var Snake = {
         PlayingFieldView: function () {
             this.drawPlayingField = function () {
                 stage.addChild(playingfieldImg);
-                stage.update();
+                
             };
             this.updatePlayingField = function () {
             };
         },
         PrisonSnakeView: function () {
-            this.drawSnake = function () {
-
+            var head = new createjs.Shape();
+            
+            this.drawSnake = function (grid) {
+                for(var i = 0; i< grid.length; i++){
+                    for(var j = 0; j< grid.length; j++){
+                        if(grid[i][j] == 2){
+                            //draw head
+                            head.graphics.beginFill("green").drawRect(i*CELL, j*CELL, CELL, CELL);
+                            stage.addChild(head);
+                        }
+                    }
+                }
             }
         }
     }
