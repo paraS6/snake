83b2a51b8520d975b79adc2b6d4d55cf5a287dd5
KrasserTommy <schloemer.thomas@gmail.com>
2017-01-15 22:16:08 +0100

    Englische Texte ausgetauscht

--- js/Menue.js
+++ js/Menue.js
@@ -190,9 +190,9 @@ Snake.Menue.GameOver = function () {
 //Screen für das nächste Level
 Snake.Menue.NextLevel = function () {
     var _nextLevelButton = new Snake.Menue.Buttons("img/naechstes_level.png", 0, 0); //Startbutton erstellen
-    var _nextLevelWindow = new Snake.Menue.StartMenue().menueWindow("Level Succeeded!", 0, 0); //Fenster erstellen für Zwischensequenzen der Levels
+    var _nextLevelWindow = new Snake.Menue.StartMenue().menueWindow("Ausbruch erfolgreich!", 0, 0); //Fenster erstellen für Zwischensequenzen der Levels
     var _firstLevelButton = new Snake.Menue.Buttons("img/spiel_neu_starten.png", 0, 0); //Startbutton erstellen um von vorne beginnen zu können
-    var _victoryWindow = new Snake.Menue.StartMenue().menueWindow("Congratulations!", 0, 0); //Fenster erstellen für Sieg-Screen
+    var _victoryWindow = new Snake.Menue.StartMenue().menueWindow("Du bist ein \rAusbrecherkoenig!", 0, 0); //Fenster erstellen für Sieg-Screen
 
     var _highScore = new Snake.Menue.Buttons("img/highscore_button.png",0, 100); //zurueck Button erstellen
 
@@ -235,7 +235,6 @@ Snake.Menue.NextLevel = function () {
         }
         // wenn das Spiel siegreich beendet wurde...
         else {
-            // !!! neuer Button mit neuem Text (zB "Back To The Beginning") erforderlich !!!
             // Highscore-Button muss noch hinzugefügt werden
             stage.addChild(_victoryWindow, _firstLevelButton, _highScore);
 
